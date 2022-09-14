e7179ab75c9fc27341913911c6e60c3b0b1efcaf
Adewunmi Oladele <87828937+Rachamv@users.noreply.github.com>
2022-09-14 21:08:32 +0100

    moving on

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use PhpParser\Node\NullableType;
 
 return new class extends Migration
 {
@@ -16,6 +17,13 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('phone')->nullable();
+            $table->string('address')->nullable();
+            $table->boolean('is_admin')->default(0);
+            $table->boolean('is_deleted')->default(0);
+            $table->boolean('is_verified')->default(0);
+            $table->boolean('is_banned')->default(0);
+            $table->integer('rank')->default(0);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
